0df737a97838951222658f8300721d0a80c2bd28
mahatirnajar <mahatirnajar@gmail.com>
2020-07-28 15:10:49 +0700

    reset password

--- todaApp/__init__.py
+++ todaApp/__init__.py
@@ -1,8 +1,10 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_migrate import Migrate
+from flask_mail import Mail
 
 app = Flask(__name__)
 app.config['SECRET_KEY']  = '5791628bb0b13ce0c676dfde280ba235'
@@ -11,6 +13,14 @@ db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
 login_manager.login_view='login'
+
 migrate = Migrate(app, db)
+
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 465 #587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('EMAIL_USER')
+app.config['MAIL_PASSWORD'] = os.environ.get('EMAIL_PASS')
+mail = Mail(app)
 
 from todaApp import routes
--- todaApp/routes.py
+++ todaApp/routes.py
@@ -2,11 +2,12 @@ import os
 import secrets
 from PIL import Image
 from flask import render_template, request, redirect, url_for, abort
-from todaApp import app, db, bcrypt
+from todaApp import app, db, bcrypt, mail
 from todaApp.models import Task, Project, User
-from todaApp.forms import NewProjectForm, NewTaskForm, RegistrationForm, loginForm, UpdateAccountForm
+from todaApp.forms import (NewProjectForm, NewTaskForm, RegistrationForm, loginForm, 
+                            UpdateAccountForm, RequestResetForm, ResetPasswordForm)
 from flask_login import login_user, current_user, logout_user, login_required
-
+from flask_mail import Message
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
@@ -176,3 +177,42 @@ def account():
 
     image_file = url_for('static', filename='img/profile_pict/'+current_user.image_file)
     return render_template('account.html', image_file=image_file, form=form)
+
+
+def send_reset_email(user):
+    token = user.get_reset_token()
+    msg = Message('Password Reset Request',
+                  sender='abdulmahatir@gmail.com',
+                  recipients=[user.email])
+    msg.body = f'''To reset your password, visit the following link:
+{url_for('reset_token', token=token, _external=True)}
+
+If you did not make this request then simply ignore this email and no changes will be made.
+'''
+    mail.send(msg)
+
+@app.route("/reset_password", methods=['GET', 'POST'])
+def reset_request():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    form = RequestResetForm()
+    if form.validate_on_submit():
+        user=User.query.filter_by(email=form.email.data).first()
+        send_reset_email(user)
+        return redirect(url_for('login'))
+    return render_template('reset_request.html', form=form)
+
+@app.route("/reset_password/<token>", methods=['GET', 'POST'])
+def reset_token(token):
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+    user = User.verify_reset_token(token)
+    if user is None:
+        return redirect(url_for('reset_request'))
+    form = ResetPasswordForm()
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user.password = hashed_password
+        db.session.commit()
+        return redirect(url_for('login'))
+    return render_template('reset_token.html', form=form)
